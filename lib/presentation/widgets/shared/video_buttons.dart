9303b19a0087400b6ce427001ee8380b0001894f
Jose Llanas <josellanasmota86@gmail.com>
2023-07-27 13:25:49 +0200

    spins icons with animate.do

--- lib/presentation/widgets/shared/video_buttons.dart
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/config/theme/helpers/human_formats.dart';
 import 'package:toktik/domain/entities/video_post.dart';
@@ -15,11 +16,17 @@ class VideoButtons extends StatelessWidget {
           value: video.likes,
           iconColor: Colors.red,
           iconData: Icons.favorite,
-        ),
+        ), const SizedBox( height: 20),
         _CustomIconButton(
           value: video.views,
           iconData: Icons.remove_red_eye_outlined,
-        )
+        ), const SizedBox( height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: const _CustomIconButton(
+            value: 0, 
+            iconData: Icons.play_circle_outline)),
       ],
     );
   }
@@ -43,6 +50,7 @@ class _CustomIconButton extends StatelessWidget {
         IconButton(
           onPressed: () {},
           icon: Icon(iconData, color: color, size: 30)),
+        if (value > 0)
         Text( HumanFormats.humanReadbleNumber(value.toDouble())),
       ],
     );
