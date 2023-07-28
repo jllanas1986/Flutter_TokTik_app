29097ec4e12868066307f2a88191732939ce24d9
Jose Llanas <josellanasmota86@gmail.com>
2023-07-28 10:55:03 +0200

    implementations

--- lib/domain/repositories/video_posts_repository.dart
+++ lib/domain/repositories/video_posts_repository.dart
@@ -1,9 +1,12 @@
 import 'package:toktik/domain/entities/video_post.dart';
 
+
+
 abstract class VideoPostRepository {
 
   Future<List<VideoPost>> getFavoriteVideosByUser( String userID );
-  
-  Future<List<VideoPost>> getTrendingVideosByPage( int page);
+
+  Future<List<VideoPost>> getTrendingVideosByPage( int page );
+
 
 }
